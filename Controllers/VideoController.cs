d5d78a9d5d5073d0cf3563b03d2e59cd33af02b6
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-04 11:11:42 -0500

    ADD Lista Videos

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+using MVCLaboratorio.Models;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LIIGabriel()
+        {
+            //obtener la info de los videos de la BD
+            DataTable dtVideos;
+            dtVideos = BaseHelper.ejecutarConsulta("sp_Video_ConsultarTodo", CommandType.StoredProcedure);
+
+            List<Video> lstVideos = new List<Video>();
+
+            //convertir el DataTable a una lista de videos List<Video>
+            foreach (DataRow item in dtVideos.Rows)
+            {
+                Video videoAux = new Video();
+                videoAux.IdVideo = int.Parse(item["IdVideo"].ToString());
+                videoAux.Nombre = item["Nombre"].ToString();
+                videoAux.Url = item["Url"].ToString();
+                videoAux.FechaPublicacion = DateTime.Parse(item["FechaPublicacion"].ToString());
+
+                lstVideos.Add(videoAux);
+            }
+            
+            return View(lstVideos);
+
+        }
+
+
+    }
+}

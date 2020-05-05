51ef9d23d33798672b31db2d607f1a46e8e62924
Max Narro <max.narro@gmail.com>
2020-05-05 17:07:13 -0500

    Video de base de datos MaxNarro

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -278,6 +278,26 @@ namespace MVCLaboratorio.Controllers
 
             return View(lstVideos);
             }
+        public ActionResult MaxNarro()
+        {
+            //obtener info de videos de la base de datos 
+            DataTable dtVideos;
+            dtVideos = BaseHelper.ejecutarConsulta("sp_Video_ConsultarTodo", CommandType.StoredProcedure);
+            List<Video> lstVideos = new List<Video>();
+
+            foreach (DataRow item in dtVideos.Rows)
+            {
+                Video videoAux = new Video();
+                videoAux.IdVideo = int.Parse(item["IdVideo"].ToString());
+                videoAux.Nombre = item["Nombre"].ToString();
+                videoAux.Url = item["Url"].ToString();
+                videoAux.FechaPublicacion = DateTime.Parse(item["FechaPublicacion"].ToString());
+
+                lstVideos.Add(videoAux);
+            }
+            return View(lstVideos);
+
+        }
         }
     }
 

b6641efd419d6c494252b7c53efb6a91a58a4a93
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-13 11:24:30 -0500

    Interface y repo

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -134,12 +134,20 @@ namespace MVCLaboratorio.Controllers
         }
 
 
-        //[HttpPost]
-        //public ActionResult LIIGabrielEdit(int id)
-        //{
+        [HttpPost]
+        public ActionResult LIIGabrielEdit(int id, Video datosVideo)
+        {
+            //realizar el update
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+            parametros.Add(new SqlParameter("@Nombre", datosVideo.Nombre));
+            parametros.Add(new SqlParameter("@Url", datosVideo.Url));
+            parametros.Add(new SqlParameter("@FechaPublicacion", datosVideo.FechaPublicacion));
+            
+            BaseHelper.ejecutarConsulta("sp_Video_Actualizar", CommandType.StoredProcedure, parametros);
 
-        //    return View();
-        //}
+            return RedirectToAction("LIIGabriel");
+        }
 
 
         //Metodo de alfonsso09

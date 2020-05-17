62fcd79a9356bf6c82dc0bfc587eb129d15a3401
Francisco Israel Hernandez Gonzalez <francisco_israel1@hotmail.com>
2020-05-17 14:13:25 -0500

    Editar completo

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -1003,8 +1003,19 @@ namespace MVCLaboratorio.Controllers
                 return View("Error");
             }
         }
+        [HttpPost]
+        public ActionResult Francisco420Edit(int id, Video datosVideo)
+        {
+            //Update
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdVideo", id));
+            parametros.Add(new SqlParameter("@Nombre", datosVideo.Nombre));
+            parametros.Add(new SqlParameter("@Url", datosVideo.Url));
+            parametros.Add(new SqlParameter("@FechaPublicacion", datosVideo.FechaPublicacion));
 
-
+            BaseHelper.ejecutarConsulta("sp_Video_Actualizar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("Francisco420");
+        }
         //Controlador de Cristian
 
         public ActionResult CristianGzz()

7eeb13281bde262abec7cb89efa5de2f37ccb284
Max Narro <max.narro@gmail.com>
2020-05-17 19:32:36 -0500

    MaxNarro edit - guardar

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -2739,6 +2739,21 @@ namespace MVCLaboratorio.Controllers
 
         }
 
+        [HttpPost]
+        public ActionResult MaxNarroEdit(int id, Video datosVideo)
+        {
+            //Realizar el update
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", id));
+            parametros.Add(new SqlParameter("@Nombre", datosVideo.Nombre));
+            parametros.Add(new SqlParameter("@Url", datosVideo.Url));
+            parametros.Add(new SqlParameter("@FechaPublicacion", datosVideo.FechaPublicacion));
+
+            BaseHelper.ejecutarConsulta("sp_Video_Actualizar", CommandType.StoredProcedure, parametros);
+
+            return RedirectToAction("MaxNarro");
+        }
+
 
 
 
